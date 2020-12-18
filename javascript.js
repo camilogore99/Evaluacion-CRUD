f89bd34767a714da988f71c9d3e4e860671d30e7
camilo <camilogore99@gmail.com>
2020-12-18 18:44:46 -0500

    initial

---
+++ javascript.js
@@ -0,0 +1,102 @@
+let cars = [
+    {
+        id: 1,
+        marca: ' MAZDA-CX-5',
+        modelo: "Gran-Turin",
+        color: 'Blanca',
+        año: "2021",
+        precio: '120.000.00'
+    },
+    {
+        id: 2,
+        marca: ' MAZDA-CX4',
+        modelo: "Turin",
+        color: 'Negra',
+        año: "2018",
+        precio: '110.000.00'
+    },
+    {
+        id: 3,
+        marca: ' MAZDA-CX2',
+        modelo: "Gran-Turin",
+        color: 'Roja',
+        año: "2016",
+        precio: '70.000.00'
+    },
+    {
+        id: 4,
+        marca: ' MAZDA-CX-1',
+        modelo: "Gran-Turin-Each",
+        color: 'Blanca',
+        año: "2015",
+        precio: '150.000.00'
+    },
+    {
+        id: 5,
+        marca: ' MAZDA-CX-3',
+        modelo: "Gran-Turin",
+        color: 'Negra',
+        año: "2019",
+        precio: '90.000.00'
+    }
+];
+function printCars() {
+      // 1. Obtengo el elemento html en el que quiero poner los carros
+    // 2. Genero el html de los carros
+    // 3. Pongo el html en el elemento obtenido
+      const container = document.getElementById("container-cars")
+      let html = '';
+      cars.forEach((car) =>{
+            html += `<tr>
+                    <td>${car.marca}</td>
+                    <td>${car.modelo}</td>
+                    <td>${car.color}</td>
+                    <td>${car.año}</td>
+                    <td>${car.precio}</td>
+                    <td>
+                        <button onclick="deleteCars(${car.id})" class="btn btn-danger">
+                            Eliminar
+                        </button>
+                    </td>
+                </tr>`;
+      });
+      console.log(cars)
+      container.innerHTML = html;
+}
+
+function deleteCars(id){
+      // Como elimino un valor de un arreglo? 1. pop -> shift -> splice
+    // necesito el índice -> ¿cómo obtengo el índice del elemento si lo que yo recibo es el id? -> findIndex
+    const index = cars.findIndex((car) =>car.id == id);
+    cars.splice(index,1);
+
+    printcars();
+}
+function addCars() {
+    // obtner el valor del input
+    // agregar el usuario al arreglo
+    // imprimo nuevamente los usuarios
+    const modelo = document.getElementById('modelo').value;
+    const id = cars[cars.length -1].id + 1;
+
+    const inputMarca = document.getElementById('marca');
+    const Marca = inputMarca.value;
+
+    const color = document.getElementById("color");
+    
+    const newCars = {
+          id:"",
+        marca: '',
+        modelo: '',
+        color: '',
+        año: "",
+        precio: ''
+        
+    }
+    cars.push(newCars);
+    printcars();
+
+    // limpiamos el formulario
+    document.getElementById('form-user').reset();
+}
+printCars();
